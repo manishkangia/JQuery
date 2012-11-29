65fcbd0cec1982b9fb15ff0183f6a875e35aaa7f
ShubhamGupta <shubham.gupta@vinsol.com>
2012-11-29 15:00:47 +0530

    secondexercise

---
+++ exercise2/js/sandbox.js
@@ -0,0 +1,19 @@
+$(document).ready(function() { 
+                               $("img[alt]").each(function () {
+                                   console.log(this.alt);
+                               });
+                               
+                               $('#search input:text').parent().addClass('new');
+                               $listItem = $("#myList .current");
+                               $listItem.next().addClass('current');
+                               $listItem.removeClass('current');
+                             
+                               $("#specials select").parent().next().children('input').css("background","red");
+                               
+                               $firstLi = $("#slideshow li:first");
+                               $firstLi.addClass('current');
+                               $firstLi.siblings().addClass('current');
+                               
+                             });
+                             
+                             

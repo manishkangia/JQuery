ee744004bf9181c5e3d1d47de98cc69443b46245
ShubhamGupta <shubham.gupta@vinsol.com>
2012-12-04 17:45:58 +0530

    exercise10

--- exercise10/js/specials.js
+++ exercise10/js/specials.js
@@ -1,12 +1,12 @@
-$newDiv = $('<div></div>');
-$newDiv.insertAfter($('#specials form'));
-$select = $('#specials form select');
+$newDiv = $( '<div></div>' );
+$newDiv.insertAfter($( '#specials form' ));
+$select = $( '#specials form select' );
 var specialsDetailsJSON;
 
 //using the 'one' function on the select menu 
 //the first funtion which is called only once performs the ajax request
 //the second function will be called all other times and even the first time to fill the div with the required content
-$select.one("change",function() {
+$select.one( "change", function() {
     $.ajax({
        url : 'data/specials.json', // url
        type : 'GET',               // request type
@@ -18,25 +18,11 @@ $select.one("change",function() {
     });
     //the second function
     $select.change( function () {
-        var selectedValue = $select.val();
-        if(selectedValue == "monday" ) {
-           $newDiv.html(specialsDetailsJSON.monday.title + '<br><br>' + specialsDetailsJSON.monday.text);
-           $newDiv.css({"color":specialsDetailsJSON.monday.color});
-           var url = "url('" + specialsDetailsJSON.monday.image + "')";
-           $newDiv.css({ "background-image" : url});
-        }
-        else if( selectedValue == "tuesday" ) {
-           $newDiv.html(specialsDetailsJSON.tuesday.title + '<br><br>' + specialsDetailsJSON.tuesday.text);
-           $newDiv.css("color",specialsDetailsJSON.tuesday.color);
-           var url = "url('" + specialsDetailsJSON.tuesday.image + "')";
-           $newDiv.css({ "background-image" : url});
-        }
-        else if( selectedValue == "friday" ) {
-           $newDiv.html(specialsDetailsJSON.friday.title + '<br><br>' + specialsDetailsJSON.friday.text);
-           $newDiv.css("color",specialsDetailsJSON.friday.color);
-           var url = "url('" + specialsDetailsJSON.friday.image + "')";
-           $newDiv.css({ "background-image" : url});
-        } 
+        var selectedValue = $select.val(); 
+        $newDiv.html(specialsDetailsJSON[selectedValue].title + '<br><br>' + specialsDetailsJSON[selectedValue].text);
+        $newDiv.css({"color":specialsDetailsJSON[selectedValue].color});
+        var url = "url('" + specialsDetailsJSON[selectedValue].image + "')";
+        $newDiv.css({ "background-image" : url});
     });
 });
 

9da432fd4f4401096f4bc6bfb81c713ae8ee6401
ShubhamGupta <shubham.gupta@vinsol.com>
2012-12-06 11:37:12 +0530

    updated

--- hidEx1/productGrid.js
+++ hidEx1/productGrid.js
@@ -1,8 +1,6 @@
 //JSON object
 var myJSONObject ;
-var selectedbrand;
-var selectedcolor;
-var checkAvailability;
+//ajax request
 $.ajax({
         url : 'products.json' ,
         type : 'GET',
@@ -10,11 +8,11 @@ $.ajax({
         success : function(response) { 
             myJSONObject = response ;
         }
-    
 });
 
 $(function () {
     var list = myJSONObject;
+    //display all initially in various divs
     for (var i = 0; i < list.length; i++) {
         var $newDiv = $( '<div class=ad style=float:left></div>' );
         src = "images/" + list[i].url;
@@ -23,60 +21,38 @@ $(function () {
         $newDiv.data( "object", myJSONObject[i]);
         $( '#container' ).append($newDiv);
     }
-    
-    $( '#selectbrand li input' ).click(function() {        
+    //assigning a function on click of each parameter
+    $( '#parameters ul li input' ).click( function() {
         updateTable();
     });
     
-    $( '#selectcolor li input' ).click(function() {
-        updateTable();
-    
-    });
-    
-    $( '#toggler' ).click(function() {
-        updateTable();
-    });
-    
+    //function to update the table 
     function updateTable() {
-        var selectedBrands = [];
-        var selectedColors = [];
         var $divlist = $( 'div.ad' );
-        $divlist.hide();
+        var selectedParameter = [];
         
-        
-        $( '#selectbrand li input' ).each( function() {
-            if ( $(this).attr('checked') ) {
-                selectedBrands.push( $(this).val() );
+    //collecting the values which are checked in each list of parameters(brands,color,availability) and displaying only those divs
+        $( '#parameters ul' ).each( function () {
+            $(this).find('li input' ).each( function() {
+                if ( $(this).attr( 'checked' )) {
+                     selectedParameter.push($(this).val());
+                } 
+            });
+            //if none selected then do nothing
+            if ( selectedParameter.length == 0) {
+                $divlist.show();
+            }
+            //else show only selected divs and then update the div list to work only on the visible divs for next iteration
+            else {
+                $divlist.hide();
+                showdivs( $divlist, selectedParameter, $(this).attr('class') );
+                selectedParameter=[];
+                $divlist = $( 'div#container div.ad:visible' );
             }
         });
-        
-        if ( selectedBrands.length == 0 ) {
-        $divlist.show();
-        }
-        else {
-        showdivs( $divlist, selectedBrands, "brand" );
-        }
-        
-        $divlist = $( 'div#container div.ad:visible' );
-        
-        $( '#selectcolor li input' ).each( function() {
-            if ( $(this).attr( 'checked' )) {
-                selectedColors.push($(this).val());
-                $divlist.hide();
-            } 
-        });
-        showdivs( $divlist, selectedColors, "color" );
-        
-        $divlist = $( 'div#container div.ad:visible' );
-        
-        checkAvailability = $( '#toggler' ).attr( 'checked' );
-        if ( checkAvailability ) {
-            $divlist.hide();
-            showdivs( $divlist, ["0"], "sold_out" );
-        }
-        
     }
     
+    //function to show divs that match the value that has been selected in the json object attached to them as data
     function showdivs($divs,selectedvalues,factor) {
         for ( var i = 0; i < $divs.length; i++ ) {
             $currentDiv = $divs.eq(i);
@@ -88,4 +64,3 @@ $(function () {
         }
     }
 });
-
